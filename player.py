9efb8dfae26d422441e67957a77853afb998986d
jason <xiaohemc2001@gmail.com>
2023-02-07 12:11:49 +0000

    Plan the player class.

---
+++ player.py
@@ -0,0 +1,14 @@
+class Player:
+    
+    def __init__(self):
+        pass
+
+    def track_money(self):
+        pass
+
+    def play_or_fold(self):
+        pass
+
+    def call_or_raise(self):
+        pass
+
