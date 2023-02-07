fa965adcbad59e670d4af102370b37b471012307
jason <xiaohemc2001@gmail.com>
2023-02-07 12:43:57 +0000

    Add face card converters

---
+++ game.py
@@ -0,0 +1,25 @@
+class Game:
+
+    def __init__(self):
+        pass
+
+    def play(self):
+        pass
+
+    def get_preflop(self):
+        pass
+
+    def draw_card(self):
+        pass
+
+    def get_all_cards(self):
+        conversion: Dict[int,str] = {
+                11: "J",
+                12: "Q",
+                13: "K",
+                14: "A"
+        }
+
+
+
+
