e24cbd78bbf5ddb14fa392812b0ff66749269721
jason <xiaohemc2001@gmail.com>
2023-02-11 20:30:42 +0000

    Add all the cards to the deck.

--- game.py
+++ game.py
@@ -1,7 +1,23 @@
+from typing import Dict, Set
+import random
+
 class Game:
 
     def __init__(self):
-        pass
+
+        self.lowest_num: int = 2
+        self.highest_num: int = 14
+        self.SUITS: Set[str] = set("C", "D", "H", "S")
+        self.CONVERSION: Dict[int,str] = {
+            11: "J",
+            12: "Q",
+            13: "K",
+            14: "A"
+        }
+
+        self.deck: Set[str] = set() 
+        self.add_cards_to_deck()
+
 
     def play(self):
         pass
@@ -9,16 +25,23 @@ class Game:
     def get_preflop(self):
         pass
 
-    def draw_card(self):
-        pass
+    def draw_card(self, leftover_cards: Set[str]):
+        return random.choice(leftover_cards)
 
-    def get_all_cards(self):
-        conversion: Dict[int,str] = {
-                11: "J",
-                12: "Q",
-                13: "K",
-                14: "A"
-        }
+
+    def add_cards_to_deck(self):
+
+        num: int
+        for num in range(self.lowest_num, self.highest_num + 1):
+            if num in self.CONVERSION:
+                num: str = self.CONVERSION[num]
+            suit: str
+            for suit in self.SUITS:
+                self.deck.add(suit + num)
+
+        return self.deck
+            
+        
 
 
 
